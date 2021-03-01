6655c295a92e1ac7ac7b247e664692e7bf275b8e
vncspam <63222945+vncspam@users.noreply.github.com>
2021-03-01 19:07:48 +0700

    Add files via upload

---
+++ precache-manifest.5bb15c65f4f6892583b18b73990933f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be6b72490455d627820f41cdff1613aa",
+    "url": "/vk-apps-todo/index.html"
+  },
+  {
+    "revision": "b313b6034907d9076796",
+    "url": "/vk-apps-todo/static/css/2.b5769064.chunk.css"
+  },
+  {
+    "revision": "f4bfe04667a8ad20614f",
+    "url": "/vk-apps-todo/static/css/main.c793b830.chunk.css"
+  },
+  {
+    "revision": "b313b6034907d9076796",
+    "url": "/vk-apps-todo/static/js/2.58b05a3d.chunk.js"
+  },
+  {
+    "revision": "f4bfe04667a8ad20614f",
+    "url": "/vk-apps-todo/static/js/main.085b3741.chunk.js"
+  },
+  {
+    "revision": "fb973330fbb88c3fc5db",
+    "url": "/vk-apps-todo/static/js/runtime~main.24924777.js"
+  }
+]);
